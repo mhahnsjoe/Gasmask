45fe1a0666e457f0fdd5a7ce053f9c3304cc1efe
mhahnsjoe <56585182+mhahnsjoe@users.noreply.github.com>
2020-03-08 18:30:48 +0100

    gui update and variables

--- src/application/Key.java
+++ src/application/Key.java
@@ -6,40 +6,42 @@ import java.util.Date;
 public class Key {
 	
 	private String accessLevel;
-	private String specAccess;
 	private String owner;
-	private Date validFrom;
 	private Date validTo;
 	private String keyID;
-	private Employee employees;
-	private ArrayList<Lock> locksList = new ArrayList<Lock>();
-	private KeyRegister keyRegister;
+	private Employee employee;
+	private String employeeID = employee.getpNbr();
+	private ArrayList<Lock> specAccess = new ArrayList<Lock>();
+	private String specialAccess=(specAccess.size()+" Rooms");
+
+	
 	
 	
 	//Getters and setters---------------
+	public String getEmployeeID() {
+		return employeeID;
+	}
+	public String getSpecialAccess() {
+		return specialAccess;
+	}
+	public void setSpecialAccess(String specialAccess) {
+		this.specialAccess = (specAccess.size()+" Rooms");
+	}
+	public void setEmployeeID(String employeeID) {
+		this.employeeID = employeeID;
+	}
 	public String getAccessLevel() {
 		return accessLevel;
 	}
 	public void setAccessLevel(String accessLevel) {
 		this.accessLevel = accessLevel;
 	}
-	public String getSpecAccess() {
-		return specAccess;
-	}
-	public void setSpecAccess(String specAccess) {
-		this.specAccess = specAccess;
-	}
+
 	public String getOwner() {
 		return owner;
 	}
 	public void setOwner(String owner) {
 		this.owner = owner;
-	}
-	public Date getValidFrom() {
-		return validFrom;
-	}
-	public void setValidFrom(Date validFrom) {
-		this.validFrom = validFrom;
 	}
 	public Date getValidTo() {
 		return validTo;
@@ -54,18 +56,18 @@ public class Key {
 		this.keyID = keyID;
 	}
 	
-	public Employee getEmployees() {
-		return employees;
+	public Employee getEmployee() {
+		return employee;
 	}
-	public void setEmployees(Employee employees) {
-		this.employees = employees;
+	public void setEmployee(Employee employees) {
+		this.employee = employees;
 	}
 	
 	public ArrayList<Lock> getLocksList() {
-		return locksList;
+		return specAccess;
 	}
-	public void setLocksList(ArrayList<Lock> locksList) {
-		this.locksList = locksList;
+	public void setLocksList(ArrayList<Lock> specAccess) {
+		this.specAccess = specAccess;
 	}
 	public KeyRegister getKeyRegister() {
 		return keyRegister;
